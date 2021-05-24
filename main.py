b2a1142df6e4c799f56fd9bdb3f6f668f8ea8686
Christopher Davisson <davissonchris@gmail.com>
2021-05-24 11:22:06 -0700

    print_board method added

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pprint
 
 
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("---------")
+            for j in range(len(bo)):
+                if j % 3 == 0:
+                    print(" | ", end="")
+                if j == 8:
+                    print(bo[i][j], end="\n")
+                else:
+                    print(str(bo[i][j]) + " ", end="")
+
+
 if __name__ == '__main__':
     board = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,3 +26,5 @@ if __name__ == '__main__':
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
+    printer = pprint.PrettyPrinter(width=41, compact=True)
+    printer.pprint(board)
