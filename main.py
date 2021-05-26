d16e78ae78c1e96db80d3937b59132ea169c3913
Christopher Davisson <davissonchris@gmail.com>
2021-05-26 09:02:09 -0700

    solver added, will be replaced with faster one. This is for checking the is_solved method

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def is_solved(board):
             # Checks for empty, if it is then it can't be solved
             if board[i][j] > 9 or board[i][j] < 1:
                 return False
+            # Checks the 3 X 3 box, row and column
             if not check_box(board, i, j) and not check_array(board[i]) and not check_array(column(board, j)):
                 return False
     return True
@@ -53,19 +54,63 @@ def column(board, i):
     return [row[i] for row in board]
 
 
+# backtracing code... not mine
+def solve_backtracing(board):
+    find = find_empty(board)
+    if find:
+        row, col = find
+    else:
+        return True
+    for i in range(1, 10):
+        if valid(board, (row, col), i):
+            board[row][col] = i
+            if solve_backtracing(board):
+                return True
+            board[row][col] = 0
+    return False
+
+
+# backtracing code... not mine
+def valid(board, pos, num):
+    for i in range(0, len(board)):
+        if board[pos[0]][i] == num and pos[1] != 1:
+            return False
+    for i in range(0, len(board)):
+        if board[i][pos[1]] == num and pos[1] != i:
+            return False
+
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+
+
 if __name__ == '__main__':
     game = [
-        [1, 2, 3, 1, 1, 0, 0, 0, 0],
-        [4, 5, 6, 0, 0, 0, 0, 0, 0],
-        [7, 8, 9, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
     printer = pprint.PrettyPrinter(width=41, compact=True)
+    solve_backtracing(game)
     printer.pprint(game)
-
+    print(is_solved(game))
+    game[0][0] = 2
     print(is_solved(game))
